bc2b1253edf4731da65dea0152c21e2085470268
Andre Costa <andre.andrade.costa@gmail.com>
2016-01-05 14:37:22 -0200

    Tests correction.

--- controllers/monitor/monitor_test.go
+++ controllers/monitor/monitor_test.go
@@ -1,25 +1,43 @@
 package monitor
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/andreandradecosta/rpimonitor/models"
+
 	"gopkg.in/unrolled/render.v1"
 )
 
-func Test_Index(t *testing.T) {
+func setup(t *testing.T) (*Monitor, *httptest.ResponseRecorder, *http.Request) {
 	req, err := http.NewRequest("GET", "http://teste.com/", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 	res := httptest.NewRecorder()
 	m := &Monitor{Render: render.New()}
-	m.Index(res, req)
+	return m, res, req
+}
 
-	exp := "Index"
-	act := res.Body.String()
-	if exp != act {
-		t.Fatalf("Expected %s got %s", exp, act)
+func TestIndex(t *testing.T) {
+	m, res, req := setup(t)
+	m.Index(res, req)
+	info := make(models.Info)
+	err := json.NewDecoder(res.Body).Decode(&info)
+	if err != nil {
+		t.Fatalf("Can't decode the result: %s", err)
 	}
+}
+
+func TestSnapshot(t *testing.T) {
+	m, res, req := setup(t)
+	m.Snapshot(res, req)
+	sample := &models.Sample{}
+	err := json.NewDecoder(res.Body).Decode(sample)
+	if err != nil {
+		t.Fatalf("Can't decode the result: %s", err)
+	}
+
 }
