0783ad34545c88c89d5dc7b21731701b2d9c186a
Andre Costa <andre.andrade.costa@gmail.com>
2016-01-13 19:38:01 -0200

    Using flags instead of env. vars.

--- rpimonitor.go
+++ rpimonitor.go
@@ -1,47 +1,31 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
+
+	"gopkg.in/unrolled/render.v1"
 
 	"github.com/andreandradecosta/rpimonitor/controllers/monitor"
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
 	"github.com/unrolled/secure"
-	"gopkg.in/unrolled/render.v1"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	httpsPort := os.Getenv("HTTPS_PORT")
-	if httpsPort == "" {
-		httpsPort = "443"
-	}
-	isDev, err := strconv.ParseBool(os.Getenv("IsDevelopment"))
-	if err != nil {
-		isDev = false
-	}
-	host := os.Getenv("HOST")
-	if host == "" {
-		host = "localhost"
-	}
-	cert := os.Getenv("CERT")
-	if cert == "" {
-		cert = "cert.pem"
-	}
-	key := os.Getenv("KEY")
-	if key == "" {
-		key = "key.pem"
-	}
+	host := flag.String("HOST", "localhost", "Domain")
+	httpPort := flag.String("HTTP_PORT", "8080", "HTTP port")
+	httpsPort := flag.String("HTTPS_PORT", "443", "HTTPS port")
+	isDev := flag.Bool("IsDevelopment", false, "Is Dev Env.")
+	cert := flag.String("CERT", "cert.pem", "Certification path")
+	key := flag.String("KEY", "key.pem", "Private Key path")
+	flag.Parse()
 
 	secureOptions := secure.Options{
 		SSLRedirect:           true,
-		SSLHost:               host + ":" + httpsPort,
+		SSLHost:               *host + ":" + *httpsPort,
 		STSSeconds:            315360000,
 		STSIncludeSubdomains:  true,
 		STSPreload:            true,
@@ -50,7 +34,7 @@ func main() {
 		BrowserXssFilter:      true,
 		ContentSecurityPolicy: "default-src 'self'",
 		PublicKey:             `pin-sha256="base64+primary=="; pin-sha256="base64+backup=="; max-age=5184000; includeSubdomains; report-uri="https://www.example.com/hpkp-report"`,
-		IsDevelopment:         isDev,
+		IsDevelopment:         *isDev,
 	}
 	secureMiddleware := secure.New(secureOptions)
 
@@ -66,13 +50,13 @@ func main() {
 	n.Use(negroni.HandlerFunc(secureMiddleware.HandlerFuncWithNext))
 	n.UseHandler(router)
 
-	addr := ":" + port
-	httpsAddr := ":" + httpsPort
+	addr := ":" + *httpPort
+	httpsAddr := ":" + *httpsPort
 	l := log.New(os.Stdout, "[negroni] ", 0)
-	l.Printf("listening on http://%s and https://%s", addr, httpsAddr)
+	l.Printf("listening on http://%s%s and https://%s%s", *host, addr, *host, httpsAddr)
 	// HTTP
 	go func() {
 		log.Fatal(http.ListenAndServe(addr, n))
 	}()
-	l.Fatal(http.ListenAndServeTLS(httpsAddr, cert, key, n))
+	l.Fatal(http.ListenAndServeTLS(httpsAddr, *cert, *key, n))
 }
