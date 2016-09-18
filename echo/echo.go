3bbf18e9e6c82f3585fe11df6f30d78d5ce5f4f5
Andre Costa <andre.andrade.costa@gmail.com>
2016-09-18 19:16:58 -0300

    Better organization and start of history handle.

--- echo/echo.go
+++ echo/echo.go
@@ -1,7 +1,9 @@
 package echo
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/andreandradecosta/rpimonitor"
 	"github.com/labstack/echo"
@@ -11,20 +13,20 @@ import (
 )
 
 type Server struct {
-	StatusReader rpimonitor.StatusReader
+	StatusReader  rpimonitor.StatusReader
+	SampleFetcher rpimonitor.SampleFetcher
 }
 
 func (s *Server) Start() {
 	e := echo.New()
 	e.SetLogLevel(log.ERROR)
+	e.SetDebug(true)
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "${time_rfc3339} - ${method}, ${uri}, [${status}]\n",
 	}))
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "index")
-	})
-	e.GET("/status", s.status)
-	e.Run(standard.New(":8080"))
+	e.GET("/", s.status)
+	e.GET("/history", s.history)
+	e.Run(standard.New("localhost:8080"))
 }
 
 func (s *Server) status(c echo.Context) error {
@@ -33,4 +35,14 @@ func (s *Server) status(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
 	return c.JSON(http.StatusOK, status)
+}
+
+func (s *Server) history(c echo.Context) error {
+	start, errS := time.Parse("2006-01-02", c.QueryParam("start"))
+	end, errE := time.Parse("2006-01-02", c.QueryParam("end"))
+	if errS != nil || errE != nil {
+		return c.String(http.StatusOK, "Invalid date range")
+	}
+	s.SampleFetcher.Query(start, end)
+	return c.String(http.StatusOK, fmt.Sprintf("Result for %v - %v", start, end))
 }
