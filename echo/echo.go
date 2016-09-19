c516ebacac36b70b07667764b53031288fbcfc2f
Andre Costa <andre.andrade.costa@gmail.com>
2016-09-19 16:01:45 -0300

    Static content dir added.

--- echo/echo.go
+++ echo/echo.go
@@ -1,10 +1,6 @@
 package echo
 
 import (
-	"fmt"
-	"net/http"
-	"time"
-
 	"github.com/andreandradecosta/rpimonitor"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/standard"
@@ -24,25 +20,8 @@ func (s *Server) Start() {
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "${time_rfc3339} - ${method}, ${uri}, [${status}]\n",
 	}))
+	e.Static("/about", "static")
 	e.GET("/", s.status)
 	e.GET("/history", s.history)
-	e.Run(standard.New("localhost:8080"))
-}
-
-func (s *Server) status(c echo.Context) error {
-	status, err := s.StatusReader.ReadStatus()
-	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
-	}
-	return c.JSON(http.StatusOK, status)
-}
-
-func (s *Server) history(c echo.Context) error {
-	start, errS := time.Parse("2006-01-02", c.QueryParam("start"))
-	end, errE := time.Parse("2006-01-02", c.QueryParam("end"))
-	if errS != nil || errE != nil {
-		return c.String(http.StatusOK, "Invalid date range")
-	}
-	s.SampleFetcher.Query(start, end)
-	return c.String(http.StatusOK, fmt.Sprintf("Result for %v - %v", start, end))
+	e.Run(standard.New(":8080"))
 }
