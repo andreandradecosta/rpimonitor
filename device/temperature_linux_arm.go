426866ffd39f447fe630ca852c05dc0f4c3feedf
Andre Costa <andre.andrade.costa@gmail.com>
2016-09-20 13:05:30 -0300

    Smalls corrections

--- device/temperature_linux_arm.go
+++ device/temperature_linux_arm.go
@@ -9,7 +9,7 @@ func GetTemperature() (string, error) {
 	cmd := exec.Command("cat", "/sys/class/thermal/thermal_zone0/temp")
 	output, err := cmd.Output()
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	return parseTemperature(output)
+	return parseTemperature(string(output))
 }
