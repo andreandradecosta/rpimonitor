fb8f96c3e3768ebfeaccbc03bbcdd325f151713d
Andre Costa <andre.andrade.costa@gmail.com>
2016-09-21 14:53:13 -0300

    Added name and interface for fetch and auth.

--- user.go
+++ user.go
@@ -1,5 +1,15 @@
 package rpimonitor
 
+import "errors"
+
 type User struct {
-	Name string
+	Login string
+	Name  string
+}
+
+var NotFound = errors.New("User not found")
+
+type UserManager interface {
+	Fetch(login string) (User, error)
+	Authenticate(login, password string) (bool, error)
 }
