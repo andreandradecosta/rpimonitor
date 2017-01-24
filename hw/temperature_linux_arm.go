40511aea85dd3e8aeb0b1a9ffea82c7cb9790d9a
Andre Costa <andre.andrade.costa@gmail.com>
2017-01-24 08:24:12 -0200

    Correction for return error on arm.

--- hw/temperature_linux_arm.go
+++ hw/temperature_linux_arm.go
@@ -13,7 +13,7 @@ func GetTemperature() (float64, error) {
 	cmd := exec.Command("/opt/vc/bin/vcgencmd", "measure_temp")
 	output, err := cmd.Output()
 	if err != nil {
-		return "", errors.Wrap(err, "could not read temperature")
+		return 0, errors.Wrap(err, "could not read temperature")
 	}
 	return parseTemperature(string(output))
 }
