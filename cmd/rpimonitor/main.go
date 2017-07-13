deedc6d5991998462b8572f60e26e7dcf3138e0f
Andre Costa <andre.andrade.costa@gmail.com>
2017-07-13 16:15:18 -0300

    Reads cert and key paths from config

--- cmd/rpimonitor/main.go
+++ cmd/rpimonitor/main.go
@@ -26,6 +26,8 @@ func main() {
 	jwtSigningKey := flag.String("JWT_SIGNING_KEY", "", "JWT Signing Key")
 	staticDir := flag.String("STATIC_DIR", "web", "Web content dir")
 	usersFile := flag.String("USERS_DB", "users.db", "Users db file")
+	certFile := flag.String("CERT_FILE", "./cert.pem", "Certificate file path")
+	keyFile := flag.String("KEY_FILE", "./key.pem", "Key file path")
 	debug := flag.Bool("DEBUG", false, "HTTP Server debug")
 
 	flag.Parse()
@@ -42,7 +44,7 @@ func main() {
 	userService := usersdb.NewUserService(*usersFile)
 
 	log.Println("Starting HTTP server...")
-	e := echo.New(*jwtSigningKey,
+	e := echo.New(*jwtSigningKey, *certFile, *keyFile,
 		echo.WithDevice(hardware),
 		echo.WithSampleFetcher(mongo),
 		echo.WithUserManager(userService),
